ad3cb715685de1d391e00830ea77ce0b49d05c5d
censhin <censhin@gmail.com>
2016-05-25 21:38:29 -0500

    chore(server): change port to listen on

--- server.go
+++ server.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	log.Println("Starting server on port 8001.")
-	http.ListenAndServe(":8001", InitRoutes())
+	log.Println("Starting server on port 8000.")
+	http.ListenAndServe(":8000", InitRoutes())
 }
