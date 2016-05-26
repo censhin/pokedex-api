3d254ccfa34246ad7a02e830617098f5c0501a62
censhin <censhin@gmail.com>
2016-05-25 22:37:27 -0500

    fix(models): include json and bson tags to properly parse data from db

--- pokemon/models.go
+++ pokemon/models.go
@@ -5,33 +5,33 @@ type Pokemons struct {
 }
 
 type Pokemon struct {
-	Name           string    `json:"name"`
-	Number         int       `json:"number"`
-	Generation     string    `json:"generation"`
-	Types          []string  `json:"types"`
-	BaseStats      BaseStats `json:"baseStats"`
-	Genus          string    `json:"genus"`
-	Height         int       `json:"height"`
-	Weight         int       `json:"weight"`
-	CaptureRate    int       `json:"captureRate"`
-	BaseExperience int       `json:"baseExperience"`
-	Abilities      []string  `json:"abilities"`
-	EffortValues   BaseStats `json:"effortValues"`
-	EggGroups      []string  `json:"eggGroups"`
-	Evolution      Evolution `json:"evolution"`
+	Name           string                 `bson:"name" json:"name"`
+	Number         int                    `bson:"number" json:"number"`
+	Generation     string                 `bson:"generation" json:"generation"`
+	Types          []string               `bson:"types" json:"types"`
+	BaseStats      BaseStats              `bson:"baseStats" json:"baseStats"`
+	Genus          string                 `bson:"genus" json:"genus"`
+	Height         int                    `bson:"height" json:"height"`
+	Weight         int                    `bson:"weight" json:"weight"`
+	CaptureRate    int                    `bson:"captureRate" json:"captureRate"`
+	BaseExperience int                    `bson:"baseExperience" json:"baseExperience"`
+	Abilities      []string               `bson:"abilities" json:"abilities"`
+	EffortValues   map[string]interface{} `bson:"effortValues" json:"effortValues"`
+	EggGroups      []string               `bson:"eggGroups" json:"eggGroups"`
+	Evolution      Evolution              `bson:"evolution" json:"evolution"`
 }
 
 type BaseStats struct {
-	Hp             int `json:"hp"`
-	Attack         int `json:"attack"`
-	Defense        int `json:"defense"`
-	SpecialAttack  int `json:"specialAttack"`
-	SpecialDefence int `json:"specialDefense"`
-	Speed          int `json:"speed"`
+	Hp             int `bson:"hp" json:"hp"`
+	Attack         int `bson:"attack" json:"attack"`
+	Defense        int `bson:"defense" json:"defense"`
+	SpecialAttack  int `bson:"specialAttack" json:"specialAttack"`
+	SpecialDefence int `bson:"specialDefense" json:"specialDefense"`
+	Speed          int `bson:"speed" json:"speed"`
 }
 
 type Evolution struct {
-	Level  int    `json:"level"`
-	Name   string `json:"name"`
-	Number int    `json:"number"`
+	Level  int    `bson:"level" json:"level"`
+	Name   string `bson:"name" json:"name"`
+	Number int    `bson:"number" json:"number"`
 }
