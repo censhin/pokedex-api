327883a0c506a6234ffddd7045c75da21be1b4a7
censhin <censhin@gmail.com>
2016-06-02 19:23:05 -0500

    fix(routes): removing OPTIONS, because it literally did nothing here

--- routes.go
+++ routes.go
@@ -9,10 +9,10 @@ import (
 func InitRoutes() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/pokemon", pokemon.CollectionResource).Methods("GET", "POST", "OPTIONS")
-	router.HandleFunc("/pokemon/{id}", pokemon.MemberResource).Methods("GET", "PUT", "DELETE", "OPTIONS")
-	router.HandleFunc("/moves", moves.CollectionResource).Methods("GET", "OPTIONS")
-	router.HandleFunc("/moves/{id}", moves.MemberResource).Methods("GET", "OPTIONS")
+	router.HandleFunc("/pokemon", pokemon.CollectionResource).Methods("GET", "POST")
+	router.HandleFunc("/pokemon/{id}", pokemon.MemberResource).Methods("GET", "PUT", "DELETE")
+	router.HandleFunc("/moves", moves.CollectionResource).Methods("GET")
+	router.HandleFunc("/moves/{id}", moves.MemberResource).Methods("GET")
 
 	return router
 }
